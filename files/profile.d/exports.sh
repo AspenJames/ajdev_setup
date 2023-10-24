38fbe0ed5c1841c5c35ffbc9e85f6c1232ec15b0
Aspen James <hello@aspenjames.dev>
2023-10-23 19:21:08 -0700

    Install golang from go.dev/dl

--- files/profile.d/exports.sh
+++ files/profile.d/exports.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+# PATH
+######
+PATH="$PATH:$HOME/.vimpkg/bin"
+PATH="$PATH:/usr/local/go/bin"
+export PATH
+
+# Editor
+########
 export EDITOR='vim'
 export VISUAL='vim'
 
+# Misc
+######
 export BAT_THEME='Monokai Extended'
-export PATH="$PATH:$HOME/.vimpkg/bin"
